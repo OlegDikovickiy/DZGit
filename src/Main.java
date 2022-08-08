8539ec037497b84a63ed5fbf37bb2db59bf154eb
OlegDikovicky <dikovickiy526@inbox.ru>
2022-08-08 22:40:35 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] sales = {123, 23, 345, 543, 12, 6, 2345, 1499, 1};
+
+    SalesManager salesManager = new SalesManager(sales);
+
+    System.out.println("Максимальное значение: " + salesManager.max());
+  }
+}
